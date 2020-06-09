a4b61ad1c2e20475929d54af8a6c1c02ee805c93
Lucas Teixeira <sobrinho1@gmail.com>
2020-06-08 21:41:12 -0300

    terminando desafio

--- src/controller/GradesController.js
+++ src/controller/GradesController.js
@@ -17,6 +17,134 @@ class GradesController {
         return res.json(arrayGrades);
     }
 
+    async treeBestGrade(req, res) {
+        const { subject, type } = req.params;
+
+        const Grade = new GradesController();
+        let arrayGrades = await Grade.loadFile();
+
+        const filterGrade = arrayGrades.grades.filter((grade) => {
+            return grade.subject == subject && grade.type == type;
+        });
+
+        const topGrade = filterGrade.sort((a, b) => {
+            return b.value - a.value;
+        });
+
+        return res.json(topGrade.splice(0, 3));
+    }
+
+    async gradeMedia(req, res) {
+        const { subject, type } = req.params;
+
+        const Grade = new GradesController();
+        let arrayGrades = await Grade.loadFile();
+
+        const filterGrade = arrayGrades.grades.filter((grade) => {
+            return grade.subject == subject && grade.type == type;
+        });
+
+        const totalGrade = filterGrade.reduce((accumulator, grade) => {
+            return accumulator + grade.value;
+        }, 0);
+
+        const media = totalGrade / filterGrade.length;
+
+        return res.json({ msg: `Média das notas: ${media} pontos.` });
+    }
+
+    async gradeStudent(req, res) {
+        const { student, subject } = req.params;
+
+        const Grade = new GradesController();
+        let arrayGrades = await Grade.loadFile();
+
+        const filterGrade = arrayGrades.grades.filter((grade) => {
+            return grade.student == student && grade.subject == subject;
+        });
+
+        const totalGrade = filterGrade.reduce((accumulator, grade) => {
+            return accumulator + grade.value;
+        }, 0);
+
+        return res.json({ msg: `Nota total: ${totalGrade} pontos.` });
+    }
+
+    async findOne(req, res) {
+        const { id } = req.params;
+
+        const Grade = new GradesController();
+        let arrayGrades = await Grade.loadFile();
+
+        const indexGrade = await arrayGrades.grades.findIndex(
+            (grade) => grade.id == id
+        );
+
+        if (indexGrade === -1) {
+            return res.status(400).json({ error: "Id não encontrado." });
+        }
+
+        return res.json(arrayGrades.grades[indexGrade]);
+    }
+
+    async delete(req, res) {
+        const { id } = req.params;
+
+        const Grade = new GradesController();
+        let arrayGrades = await Grade.loadFile();
+
+        const indexGrade = await arrayGrades.grades.findIndex(
+            (grade) => grade.id == id
+        );
+
+        if (indexGrade === -1) {
+            return res.status(400).json({ error: "Id não encontrado." });
+        }
+
+        arrayGrades.grades.splice(indexGrade, 1);
+
+        try {
+            await fs.writeFile(global.file, JSON.stringify(arrayGrades));
+        } catch (err) {
+            res.status(400).json({ error: err });
+        }
+
+        return res.json({ msg: "Removido com sucesso." });
+    }
+
+    async update(req, res) {
+        const { id } = req.params;
+        const { student, subject, type, value } = req.body;
+
+        const Grade = new GradesController();
+        let arrayGrades = await Grade.loadFile();
+
+        const indexGrade = await arrayGrades.grades.findIndex(
+            (grade) => grade.id == id
+        );
+
+        if (indexGrade === -1) {
+            return res.status(400).json({ error: "Id não encontrado." });
+        }
+
+        arrayGrades.grades[indexGrade] = {
+            id,
+            student,
+            subject,
+            type,
+            value,
+            timestamp: new Date(),
+        };
+
+        try {
+            await fs.writeFile(global.file, JSON.stringify(arrayGrades));
+        } catch (err) {
+            res.status(400).json({ error: err });
+        }
+
+        return res.json(arrayGrades.grades[indexGrade]);
+    }
+
     async create(req, res) {
         const Grade = new GradesController();
         let arrayGrades = await Grade.loadFile();
--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,11 @@ const gradesController = new Grades();
 
 routes.get("/grades", gradesController.index);
 routes.post("/grades", gradesController.create);
+routes.put("/grades/:id", gradesController.update);
+routes.delete("/grades/:id", gradesController.delete);
+routes.get("/grades/:id", gradesController.findOne);
+routes.get("/grades/:student/:subject", gradesController.gradeStudent);
+routes.get("/grades-media/:subject/:type", gradesController.gradeMedia);
+routes.get("/grades-top/:subject/:type", gradesController.treeBestGrade);
 
 module.exports = routes;
